383aa54ca2e06dd8b3e411e40c46d22c743748e0
carloshs1 <carlos@ebless.mx>
2022-11-20 17:12:34 -0600

    Add mobile menu functionality

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Footer from './components/Footer'
 
 function App() {
  return (
-  <div className="App">
+  <div className="App overflow-hidden">
    <Header />
    <Routes>
     <Route path="/" element={<Events />} />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,52 @@
 import { Bars3BottomRightIcon, TicketIcon } from '@heroicons/react/24/solid'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Header: React.FC = () => {
+ const [mobileMenu, setMobileMenu] = useState(false)
+ const navigate = useNavigate()
+ const handleClick = (route: string) => {
+  setMobileMenu(false)
+  navigate(route)
+ }
  return (
   <header className="sticky top-0 z-50 flex justify-between items-center px-5 py-6 w-full bg-[rgba(28,28,30,.9)]">
+   <div
+    className={`absolute top-16 h-screen w-screen bg-black opacity-40 py-10 ${
+     mobileMenu ? 'left-0' : 'left-[150%]'
+    } md:hidden`}
+   />
+   <div
+    className={`absolute top-16 bg-black w-[80%] flex flex-col ${
+     mobileMenu ? 'left-[10%]' : 'left-[150%]'
+    } md:hidden`}
+   >
+    <p
+     onClick={() => handleClick('/add-event')}
+     className="text-xl py-2 text-white"
+    >
+     Create event
+    </p>
+    <hr className=" border-dashed" />
+    <p
+     onClick={() => handleClick('/login')}
+     className="text-xl py-2 text-white"
+    >
+     Log in
+    </p>
+   </div>
    <Link to={'/'} className="text-xl hover:text-white">
     <div className="flex space-x-1 items-center">
      <TicketIcon className="h-6 w-6 text-white" />
-     <p className="text-white text-sm font-thin">
+     <p className="text-white text-sm md:text-base font-thin">
       THE<span className="font-extrabold">TICKET</span>CLONE
      </p>
     </div>
    </Link>
-   <Bars3BottomRightIcon className="h-6 w-6 text-white md:hidden" />
+   <Bars3BottomRightIcon
+    className="h-6 w-6 text-white md:hidden"
+    onClick={() => setMobileMenu(!mobileMenu)}
+   />
    <div className="hidden md:flex space-x-8">
     <Link to={'/add-event'} className="text-sm">
      Create event
