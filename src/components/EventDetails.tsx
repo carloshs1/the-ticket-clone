d8b672693b6b09e5bec883bff30f6f6a278a316b
carloshs1 <carlos@ebless.mx>
2022-11-20 10:07:53 -0600

    Complete Event details component

--- src/components/EventDetails.tsx
+++ src/components/EventDetails.tsx
@@ -3,6 +3,7 @@ import { Link, useNavigate, useParams } from 'react-router-dom'
 import { createDate, createDayOfTheWeek, createTime } from '../utils/functions'
 import { EventType } from '../utils/types'
 import { TicketIcon } from '@heroicons/react/24/solid'
+import { v4 as uuid } from 'uuid'
 
 const EventDetails: React.FC = () => {
  const navigate = useNavigate()
@@ -20,13 +21,15 @@ const EventDetails: React.FC = () => {
   navigate('/')
  }
  return (
-  <div className="px-3 py-5 mx-auto space-y-3 md:w-[748px]">
-   <h1 className="text-2xl text-white text-left">{event.name}</h1>
-   <p className="text-left text-sm">{`${createDayOfTheWeek(
+  <div className="px-3 py-5 mx-auto space-y-3 md:w-[748px] lg:w-[964px]">
+   <h1 className="text-2xl text-white text-left md:text-3xl md:font-medium">
+    {event.name}
+   </h1>
+   <p className="text-left text-sm md:text-xl">{`${createDayOfTheWeek(
     event.date
    )}, ${createDate(event.date, false)} ,${createTime(event.date)}`}</p>
    <div className="flex flex-col space-y-5 md:flex-row md:space-x-5 md:space-y-0">
-    <div className="flex flex-col gap-5 md:max-w-[228px] md:flex-col-reverse md:space-y-0">
+    <div className="flex flex-col gap-5 md:max-w-[228px] lg:max-w-[300px] md:flex-col-reverse md:space-y-0">
      <div className="border-white border-2">
       <img src={event.flyer} alt="Flyer" className="w-full" />
      </div>
@@ -57,13 +60,22 @@ const EventDetails: React.FC = () => {
       </div>
      </div>
     </div>
-    <div className="md:max-w-[476px] space-y-5">
-     <div className="bg-white md:h-min">
-      <p className="text-gray-500 text-left text-sm p-5">{event.description}</p>
+    <div className="w-full md:max-w-[476px] lg:max-w-[620px] space-y-5">
+     <div className="bg-white p-5 md:h-min">
+      {event.description?.split('\n').map((line) =>
+       !line ? (
+        <p key={uuid()} className="h-5" />
+       ) : (
+        <p key={uuid()} className="text-gray-500 text-left text-sm">
+         {line}
+        </p>
+       )
+      )}
+      {/* <p className="text-gray-500 text-left text-sm p-5">{event.description}</p> */}
      </div>
      <div className="flex justify-between md:justify-end md:space-x-3">
       <Link to={`/event/${id}/edit`} className="text-sm">
-       <button className="text-sm border border-gray-300 py-[8px] hover:py-[9px] px-[12px] hover:px-[13px] hover:border-none rounded-sm hover:bg-gradient-to-r hover:from-orange-300 hover:to-red-500">
+       <button className="text-sm border border-gray-300 py-[8px] hover:py-[9px] px-[12px] hover:px-[13px] hover:border-none rounded-sm hover:bg-gradient-to-r hover:from-orange-300 hover:to-red-500 md:text-base">
         Edit your event
        </button>
       </Link>
